35de41993ee0892ad1d92718b0fd9aeba81d1987
Andra Meedun <irev@users.noreply.github.com>
2020-08-31 20:48:06 +0700

    move layout to view

---
+++ app/view/layout/ligatcode_pagination.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @var \CodeIgniter\Pager\PagerRenderer $pager
+ */
+//Set number line of Page
+$pager->setSurroundCount(5);
+?>
+<nav aria-label="<?= lang('Pager.pageNavigation') ?>">
+    <ul class="pagination">
+        <?php if ($pager->hasPrevious()) : ?>
+            <li class="page-item">
+                <a href="<?= $pager->getPrevious() ?>" class="page-link" aria-label="<?= lang('Pager.previous') ?>">
+                    <span>«</span>
+                </a>
+            </li>
+        <?php endif ?>
+
+        <?php foreach ($pager->links() as $link) : ?>
+            <li <?= $link['active'] ? 'class="active page-item"' : 'class="page-item"' ?>>
+                <a href="<?= $link['uri'] ?>" class="page-link">
+                    <?= $link['title'] ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+
+        <?php if ($pager->hasNext()) : ?>
+            <li class="page-item">
+                <a href="<?= $pager->getNext() ?>" aria-label="<?= lang('Pager.next') ?>" class="page-link">
+                    <span aria-hidden="true">»</span>
+                </a>
+            </li>
+        <?php endif ?>
+    </ul>
+</nav>
