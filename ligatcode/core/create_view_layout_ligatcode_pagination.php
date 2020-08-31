5e30f409097adc59b57004a9f8b7e00c4632f269
Andra Meedun <irev@users.noreply.github.com>
2020-09-01 05:21:48 +0700

    sidebar

---
+++ ligatcode/core/create_view_layout_ligatcode_pagination.php
@@ -0,0 +1,38 @@
+<?php
+$string ="
+<?php
+/**
+ * @var \CodeIgniter\Pager\PagerRenderer \$pager
+ */
+//Set total number to show previuse and net 
+\$pager->setSurroundCount(1);
+?>
+<nav aria-label=\"<?= lang('Pager.pageNavigation') ?>\">
+    <ul class=\"pagination\">
+        <?php if (\$pager->hasPrevious()) : ?>
+            <li class=\"page-item\">
+                <a href=\"<?= \$pager->getPrevious() ?>\" class=\"page-link\" aria-label=\"<?= lang('Pager.previous') ?>\">
+                    <span>«</span>
+                </a>
+            </li>
+        <?php endif ?>
+
+        <?php foreach (\$pager->links() as \$link) : ?>
+            <li <?= \$link['active'] ? 'class=\"active page-item\"' : 'class=\"page-item\"' ?>>
+                <a href=\"<?= \$link['uri'] ?>\" class=\"page-link\">
+                    <?= \$link['title'] ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+
+        <?php if (\$pager->hasNext()) : ?>
+            <li class=\"page-item\">
+                <a href=\"<?= \$pager->getNext() ?>\" aria-label=\"<?= lang('Pager.next') ?>\" class=\"page-link\">
+                    <span aria-hidden=\"true\">»</span>
+                </a>
+            </li>
+        <?php endif ?>
+    </ul>
+</nav>";
+
+$result_view_layout_ligatcode_pagination = createFile($string, $target . "views/layout/ligatcode_pagination.php");
